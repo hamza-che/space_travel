3fba84af68d8f56d2801dbd5f9b4b72a68e9bba3
hamza-che <chenguitiha@gmail.com>
2022-07-31 16:12:00 +0100

    add css general reset and utility classes

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="flex call-to-action container">
+      <div className="box">a</div>
+      <div className="box">b</div>
+      <div className="box">c</div>
+      <div className="box">d</div>
+    </div>
+  );
+}
+
+export default App;
